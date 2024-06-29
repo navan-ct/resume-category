4c07a808f618a5605baad7a85430d7f7543c430e
Navaneeth CT <navaneethct1199@gmail.com>
2024-06-29 13:54:59 +0530

    Update Prettier configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,16 @@
-import compression from "compression";
-import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+import compression from 'compression'
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
-const app = express();
-app.use(morgan("tiny"));
-app.disable("x-powered-by");
-app.use(helmet());
-app.use(compression());
+const app = express()
+app.use(morgan('tiny'))
+app.disable('x-powered-by')
+app.use(helmet())
+app.use(compression())
 
-app.get("/health", (_request, response) => {
-  response.sendStatus(200);
-});
+app.get('/health', (_request, response) => {
+  response.sendStatus(200)
+})
 
-export default app;
+export default app
--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,28 +1,28 @@
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, transports } from 'winston'
 
 const consoleFormat = format.combine(
-  format.timestamp({ format: "isoDateTime" }),
+  format.timestamp({ format: 'isoDateTime' }),
   format.printf((info) => {
-    return `[${info["timestamp"]}] [${info.level.toUpperCase()}] ${info.message}`;
+    return `[${info['timestamp']}] [${info.level.toUpperCase()}] ${info.message}`
   }),
   format.colorize({
     all: true,
     colors: {
-      error: "red",
-      warn: "yellow",
-      info: "blue",
-      http: "magenta",
-    },
-  }),
-);
+      error: 'red',
+      warn: 'yellow',
+      info: 'blue',
+      http: 'magenta'
+    }
+  })
+)
 
 const logger = createLogger({
   transports: [
     new transports.Console({
-      level: "http",
-      format: consoleFormat,
-    }),
-  ],
-});
+      level: 'http',
+      format: consoleFormat
+    })
+  ]
+})
 
-export default logger;
+export default logger
